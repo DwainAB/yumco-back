61604f9185f94aa07182ec3c168c571271b839f2
Dwain <dwaincontact@gmail.com>
2026-04-02 16:24:21 +0200

    Feat: Add user model and add alembic

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
+from sqlalchemy.sql import func
+from app.db.database import Base
+
+class User(Base):
+    __tablename__="users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email= Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone = Column(String, nullable=True)
+    expo_push_token = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
